4a2223eb63769aaf046096c4cd706bf6c991eb25
Sergius <kashuboser@yandex.ru>
2024-11-10 16:58:32 +0300

    проба пера

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,64 @@
-package ru.practicum.dinner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class DinnerConstructor {
+    //объявления таблицы тип блюда - список блюд
+    HashMap<String, ArrayList<String>> typeDish;
+
+
+    DinnerConstructor() {
+
+        typeDish = new HashMap<>();
+
+    }
+    // метод записи типа блюд и списка блюд, принадлежащих данному типу
+    void addDish(String type, String dish) {
+
+        for (String this_type : typeDish.keySet()) {
+            //если указанный тип уже есть в таблице типов блюд
+            if (type.equals(this_type)) {
+                //добавление в список по указанному типу блюд
+                typeDish.get(this_type).add(dish);
+                return;
+            }
+        }
+        //если указанного типа блюд нет в таблице
+        if (!checkType(type)) {
+            //объявление пустого списка блюд для нового типа
+            ArrayList<String> newEat = new ArrayList<>();
+            //вставка нового типа блюд с его списоком блюд
+            typeDish.put(type, newEat);
+            //обновление списка блюд по новому типу блюд
+            typeDish.get(type).add(dish);
+        }
+        return;
+
+    }
+    //метод показывающий набор существующих в таблице типов блюд
+    void seeTypeDish() {
+        System.out.println(typeDish.keySet());
+    }
+    //метод проверки наличия указанного типа блюд в таблице
+    boolean checkType(String type) {
+
+        boolean check = false;
+
+        for (String this_type : typeDish.keySet()) {
+            if (type.equals(this_type)) {
+                check = true;
+            }
+        }
+        return check;
+    }
+    //метод случайного поиска блюда в списке указаного типа блюд
+    String randomDishType(String type) {
+        Random random = new Random();
+        //ограничение случайного числа в границе от 0 до длины списка блюд
+        int randomNumber = random.nextInt(typeDish.get(type).size());
+        //вывод случайного блюда
+        return typeDish.get(type).get(randomNumber);
+    }
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,6 +1,6 @@
-package ru.practicum.dinner;
-
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class Main {
 
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         dc = new DinnerConstructor();
         scanner = new Scanner(System.in);
-
+        //цикл обработки запроса пользователя через меню программы
         while (true) {
             printMenu();
             String command = scanner.nextLine();
@@ -27,23 +27,25 @@ public class Main {
             }
         }
     }
-
+    //метод вызова меню программы
     private static void printMenu() {
         System.out.println("Выберите команду:");
         System.out.println("1 - Добавить новое блюдо");
         System.out.println("2 - Сгенерировать комбинации блюд");
         System.out.println("3 - Выход");
     }
-
+    //метод добавления нового блюдо в меню ресторана
     private static void addNewDish() {
         System.out.println("Введите тип блюда:");
         String dishType = scanner.nextLine();
+
         System.out.println("Введите название блюда:");
         String dishName = scanner.nextLine();
+        // добавление нового блюдо
+        dc.addDish(dishType, dishName);
 
-        // добавьте новое блюдо
     }
-
+    //метод генерации комбо из блюд меню
     private static void generateDishCombo() {
         System.out.println("Начинаем конструировать обед...");
 
@@ -53,13 +55,34 @@ public class Main {
 
         System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
         String nextItem = scanner.nextLine();
+        //объявление  списка типов блюд для генерации комбо
+        ArrayList<String> listType = new ArrayList<>();
+        //объявление списка комбо, генерированного
+        ArrayList<String> listCombo = new ArrayList<>();
 
-        //реализуйте ввод типов блюд
+        //ввод типов блюд через ENTER
         while (!nextItem.isEmpty()) {
 
+            if (dc.checkType(nextItem)) {
+                listType.add(nextItem);
+            } else {
+                System.out.println("Указанный вами тип блюда нет в перечне известных типов меню : ");
+                dc.seeTypeDish();
+                System.out.println("Введите пожалуйста один из известных типов блюд");
+            }
+            nextItem = scanner.nextLine();
         }
 
-        // сгенерируйте комбинации блюд и выведите на экран
+        // генерирация комбинации блюд и вывод на экран
+        for (int i = 0; i < numberOfCombos; i++) {
+            listCombo.clear();
+            System.out.println("Комбо " + (i + 1));
+            for (String thisType : listType) {
+                listCombo.add(dc.randomDishType(thisType));
+            }
+            System.out.println(listCombo);
+        }
+
 
     }
 }
